ce473d6c2c68d89eaf7a3445dcd66e4fd902c532
Laís Lobo Teixeira <kyliegomez120.n@outlook.com>
2024-06-27 18:40:19 -0300

    main

---
+++ main.py
@@ -0,0 +1,12 @@
+# importar a base de dados
+
+# visualizar a base de dados
+
+# faturamento por loja
+
+# quantidade de produtos vendidos por loja
+
+# ticket médio por produto em cada loja
+
+# enviar um email com o relatório 
+
